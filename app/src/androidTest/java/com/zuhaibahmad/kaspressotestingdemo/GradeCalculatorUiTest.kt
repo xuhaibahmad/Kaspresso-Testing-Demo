a0188e4ddb9a75799015026a17edbff35a4e5d0a
Saygın Dikbayır <hdikbayir@gmail.com>
2021-04-23 13:22:09 +0300

    Adding some border test for calcutor screen

--- app/src/androidTest/java/com/zuhaibahmad/kaspressotestingdemo/GradeCalculatorUiTest.kt
+++ app/src/androidTest/java/com/zuhaibahmad/kaspressotestingdemo/GradeCalculatorUiTest.kt
@@ -15,7 +15,7 @@ class GradeCalculatorUiTest : TestCase() {
     val screen = GradeCalculatorScreen()
 
     @Test
-    fun onCorrectScoreSubmit_shouldDisplayCorrectGrade() = before {
+    fun onCorrectScoreSubmit_shouldDisplayCorrectGroade() = before {
         // No-op
     }.after {
         // No-op
@@ -35,4 +35,173 @@ class GradeCalculatorUiTest : TestCase() {
             }
         }
     }
+
+    @Test
+    fun whenGradeIsLessThan60GradeFMustBeShown() = run {
+        screen {
+            step("Grade Calculator Screen Opens") {
+                rule.launchActivity(null)
+            }
+            step("In TextField 59 Entered") {
+                inputMarks.replaceText("59")
+            }
+            step("Submit Button Pressed") {
+                buttonSubmit.click()
+            }
+            step("Verify the displayed grade is F") {
+                labelGrade.hasText("Your Grade is: F")
+            }
+        }
+    }
+    @Test
+    fun whenGradeIsGreaterThan60GradeDMustBeShown()= run{
+        screen{
+            step("Grade Calculator Screen Opens"){
+                rule.launchActivity(null)
+            }
+            step("In TextField 61 Entered") {
+                inputMarks.replaceText("61")
+            }
+            step("Submit Button Pressed") {
+                buttonSubmit.click()
+            }
+            step("Verify the displayed grade is D") {
+                labelGrade.hasText("Your Grade is: D")
+            }
+        }
+    }
+    @Test
+    fun whenGradeIs69GradeDMustBeShown()= run{
+        screen{
+            step("Grade Calculator Screen Opens"){
+                rule.launchActivity(null)
+            }
+            step("In TextField 69 Entered") {
+                inputMarks.replaceText("69")
+            }
+            step("Submit Button Pressed") {
+                buttonSubmit.click()
+            }
+            step("Verify the displayed grade is D") {
+                labelGrade.hasText("Your Grade is: D")
+            }
+        }
+    }
+    @Test
+    fun whenGradeIsGreaterThan70GradeDMustBeShown()= run{
+        screen{
+            step("Grade Calculator Screen Opens"){
+                rule.launchActivity(null)
+            }
+            step("In TextField 71 Entered") {
+                inputMarks.replaceText("71")
+            }
+            step("Submit Button Pressed") {
+                buttonSubmit.click()
+            }
+            step("Verify the displayed grade is C") {
+                labelGrade.hasText("Your Grade is: C")
+            }
+        }
+    }
+    @Test
+    fun whenGradeIs79GradeDMustBeShown()= run{
+        screen{
+            step("Grade Calculator Screen Opens"){
+                rule.launchActivity(null)
+            }
+            step("In TextField 79 Entered") {
+                inputMarks.replaceText("79")
+            }
+            step("Submit Button Pressed") {
+                buttonSubmit.click()
+            }
+            step("Verify the displayed grade is C") {
+                labelGrade.hasText("Your Grade is: C")
+            }
+        }
+    }
+    @Test
+    fun whenGradeIsGreatedThan80GradeBMustBeShown()= run{
+        screen{
+            step("Grade Calculator Screen Opens"){
+                rule.launchActivity(null)
+            }
+            step("In TextField 81 Entered") {
+                inputMarks.replaceText("81")
+            }
+            step("Submit Button Pressed") {
+                buttonSubmit.click()
+            }
+            step("Verify the displayed grade is B") {
+                labelGrade.hasText("Your Grade is: B")
+            }
+        }
+    }
+    @Test
+    fun whenGradeIs89GradeBMustBeShown()= run{
+        screen{
+            step("Grade Calculator Screen Opens"){
+                rule.launchActivity(null)
+            }
+            step("In TextField 89 Entered") {
+                inputMarks.replaceText("89")
+            }
+            step("Submit Button Pressed") {
+                buttonSubmit.click()
+            }
+            step("Verify the displayed grade is B") {
+                labelGrade.hasText("Your Grade is: B")
+            }
+        }
+    }
+    @Test
+    fun whenGradeIsGreaterThan90GradeBMustBeShown()= run{
+        screen{
+            step("Grade Calculator Screen Opens"){
+                rule.launchActivity(null)
+            }
+            step("In TextField 91 Entered") {
+                inputMarks.replaceText("91")
+            }
+            step("Submit Button Pressed") {
+                buttonSubmit.click()
+            }
+            step("Verify the displayed grade is A") {
+                labelGrade.hasText("Your Grade is: A")
+            }
+        }
+    }
+  /*  @Test
+    fun whenGradeNullWarningMessageShouldNeDisplayed()= run{
+        screen{
+            step("Grade Calculator Screen Opens"){
+                rule.launchActivity(null)
+            }
+            step("Submit Button Pressed Without Any Value Entered") {
+                buttonSubmit.click()
+            }
+            step("Verify the Warning Message Displayed") {
+                labelGrade.hasText("Your Grade is: Please Enter a Number to Mark Text")
+            }
+        }
+    }*/
+    @Test
+    fun whenGradeIsGreaterThan100WarningMessageShouldNeDisplayed()= run{
+        screen{
+            step("Grade Calculator Screen Opens"){
+                rule.launchActivity(null)
+            }
+            step("In TextField 101 Entered") {
+                inputMarks.replaceText("101")
+            }
+            step("Submit Button Pressed ") {
+                buttonSubmit.click()
+            }
+            step("Verify the Warning Message Displayed") {
+                labelGrade.hasText("Your Grade is: The Mark Value must be in between 0 to 100")
+            }
+        }
+    }
 }
+
--- app/src/main/java/com/zuhaibahmad/kaspressotestingdemo/GradeCalculator.kt
+++ app/src/main/java/com/zuhaibahmad/kaspressotestingdemo/GradeCalculator.kt
@@ -9,6 +9,8 @@ class GradeCalculator {
     fun getGrade(obtainedMarks: Int, totalMarks: Int): String {
         val percentage = getPercentage(obtainedMarks, totalMarks)
         return when {
+            percentage > 100 -> "The Mark Value must be in between 0 to 100"
+            percentage == null -> "Please Enter a Number to Mark Text"
             percentage >= 90 -> "A"
             percentage in 80..89 -> "B"
             percentage in 70..79 -> "C"
